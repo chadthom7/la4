f0ae86c5a88cdc6aeefee3e58ccb985bc26cfe0d
Carter Venable <cartervenable@Carters-MacBook-Pro-503.local>
2019-11-19 21:14:21 -0500

    Added variables to SendRcv

--- StudentServerStrategy.java
+++ StudentServerStrategy.java
@@ -18,8 +18,14 @@ public class StudentServerStrategy implements ServerStrategy{
     }
 
     public List<Message> sendRcv(List<Message> clientMsgs){
+        HEAD
+        int cwnd = 1;
+        int ssthresh;
+        boolean slowStart = true; // false = congestion avoidance; true = slow start
         
-        return clientMsgs; //obviously wrong
+
+
+        //return clientMsgs; //obviously wrong
 
 
     }

d313ac90c59d62727812223ec8368cc5a6fc29b3
chadthom7 <chadthom7@gmail.com>
2019-11-19 22:55:43 -0500

    layout

--- StudentServerStrategy.java
+++ StudentServerStrategy.java
@@ -1,36 +1,60 @@
 import java.util.*;
 
 public class StudentServerStrategy implements ServerStrategy{
-    List<String> file;
+		List<String> file;
 
 
-    public StudentServerStrategy(){
-        reset();
-    }
+		public StudentServerStrategy(){
+				reset();
+		}
 
-    public void setFile(List<String> file){
-        this.file = file;
-    }
+		public void setFile(List<String> file){
+				this.file = file;
+		}
 
-    public void reset(){
-
-
-    }
-
-    public List<Message> sendRcv(List<Message> clientMsgs){
-        int cwnd = 1;
-        int ssthresh = 32;
-        boolean slowStart = true; // false = congestion avoidance; true = slow start
-        
-        // Slow start
-        while(cwnd < ssthresh) {
-            
-        }
+		public void reset(){
 
 
-        //return clientMsgs; //obviously wrong
+		}
 
+	public List<Message> sendRcv(List<Message> clientMsgs){
+		int cwnd = 1;
+		int ssthresh = 32;
+		boolean timeout = false;
+		// ssthresh = ssthresh/2; cwnd = 1; slowStart = true;
+		boolean triple_dup = false;
+		// ssthresh = ssthresh/2; cwnd = ssthresh;  slowStart = false;
+		boolean slowStart = true;
+		// if false, do congestion avoidance, else exponentially increase
+		
+		for(Message m: clientMsgs){
+			acks[m.num-1] =true;
+			System.out.println(m.num+","+m.msg);
+		}
+		int firstUnACKed = 0;
+		List<Message> msgs = new ArrayList<Message>();
+			
+			
+		// Slow start
+		while(firstUnACKed < acks.length) {
+			
+			//TODO Check is congestions
+			
+			
+			// TODO increment counter
+			if(cwnd < ssthresh && slowStart) {
+				
+			}
+			else if(triple_dup) {
+				
 
-    }
+			}
+			else if(timeout) {
+			
+			}
+		}
+
+		return msgs;
+	}
     
 }
